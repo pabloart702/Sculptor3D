1007469d4d912c2b71544abb0cef676b44a46abf
pabloart702 <pabloarthur702@gmail.com>
2024-05-03 23:34:26 -0300

    Adicionado construtor e destrutor

--- sculptor.cpp
+++ sculptor.cpp
@@ -1,5 +1,26 @@
 #include "sculptor.h"
+#include <stdlib.h>
 
 Sculptor::Sculptor(int _nx, int _ny, int _nz) {
+    //Copia as dimensões
+    nx = _nx;
+    ny = _ny;
+    nz = _nz;
+    v = new Voxel**[nx];
+    for(int i=0; i<nx; i++){
+        v[i] = new Voxel*[ny];
+        for(int j=0; j<ny; j++){
+            v[i][j] = new Voxel[nz];
+        }
+    }
+}
 
+Sculptor::~Sculptor(){
+    for(int i=0; i<nx; i++){
+        for(int j=0; j<ny; j++){
+            free(v[i][j]) ;
+        }
+        free(v[i]);
+    }
+    free(v) ;
 }
